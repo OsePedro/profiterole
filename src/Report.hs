dac70c3b2f0a2a4b71d87d89c7cb0c4f1012cae6
Ose Pedro <>
2024-11-18 18:05:47 +0000

    Lines are now coloured based on call site depth.

--- src/Report.hs
+++ src/Report.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, TupleSections #-}
 
 module Report(
     reportText,
@@ -24,23 +24,50 @@ presort =
 reportHTML :: [Tree Val] -> [String]
 reportHTML vals =
     let vals2 = presort vals
-        indent i x = x{name = replicate (i*2) ' ' ++ name x}
         links = Set.fromList $ map (name . rootLabel) vals2
         anchor x = "<a id='" ++ show (hash x) ++ "'></a>"
-    in (["<pre>"] ++) $ (++ ["</pre>"]) $ intercalate [""] $
-        (" TOT   INH   IND" : map (showHTML links . rootLabel) (take 25 vals2)) :
+    in ((css ++ ["<pre>"]) ++) $ (++ ["</pre>"]) $ intercalate [""] $
+        (" TOT   INH   IND" : map (showHTML links . (0,) . rootLabel) (take 25 vals2)) :
         [ anchor (name $ rootLabel x) :
-          map (showHTML $ Set.delete (name $ rootLabel x) links) (flatten $ fmapTreeDepth indent x)
+          map (showHTML $ Set.delete (name $ rootLabel x) links) (flatten $ fmapTreeDepth (,) x)
         | x <- vals2]
 
-showHTML :: Set.Set String -> Val -> String
-showHTML xs Val{..} = intercalate "  "
-    [showDouble timeTot, showDouble timeInh, showDouble timeInd
-    ,spc ++ (if nam `Set.member` xs then link else nam) ++ " (" ++ show entries ++ ")"]
+showHTML :: Set.Set String -> (Int, Val) -> String
+showHTML xs (indent, Val{..}) =
+    (col $ intercalate "  " [showDouble timeTot, showDouble timeInh, showDouble timeInd]) ++
+    "  " ++
+    spc ++ (if name `Set.member` xs then link else colouredName) ++
+    col (" (" ++ show entries ++ ")")
     where
-        link = "<a href='#" ++ show (hash nam) ++ "'>" ++ nam ++ "</a>"
-        (spc,nam) = span isSpace name
+        link = "<a href='#" ++ show (hash name) ++ "'>" ++ colouredName ++ "</a>"
+        colouredName = col name
+        col = colourText indent
+        spc = replicate (indent * 2) ' '
 
+colourText :: Int -> String -> String
+colourText i text =
+    "<span class=\"" ++ cssColourClass i ++ "\">" ++ text ++ "</span>"
+
+css :: [String]
+css =
+    ["<style>", "body {background-color: white;}"] ++
+    (defineColourClass <$> [0 .. noCssColours - 1]) ++
+    ["</style>"]
+    where
+    defineColourClass i =
+        '.' : cssColourClass i ++ " {color: " ++ cssColour i ++ ";}"
+
+cssColourClass :: Int -> String
+cssColourClass i = "c" ++ show (rem i noCssColours)
+
+noCssColours :: Int
+noCssColours = 4
+
+cssColour :: Int -> String
+cssColour 0 = "black"
+cssColour 1 = "#e6194B"
+cssColour 2 = "#4363d8"
+cssColour 3 = "#f58231"
 
 reportText :: [Tree Val] -> [String]
 reportText vals =
